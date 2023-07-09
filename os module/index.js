443fc5a9f60150f6f263702b8825407bd747be9d
SuperSection <soumosarkar297@gmail.com>
2023-07-09 11:14:12 +0530

    learnt about os module

---
+++ os module/index.js
@@ -0,0 +1,9 @@
+const os = require('os');
+
+// getting details about the operating system
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.platform());
+console.log(os.machine());
+console.log(os.homedir());
+console.log(os.hostname());
