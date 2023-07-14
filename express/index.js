95af953b2bad57c60d3c5e00a79fdf92063f7558
SuperSection <soumosarkar297@gmail.com>
2023-07-14 10:41:24 +0530

    learnt use of Template Engine (ejs)

--- express/index.js
+++ express/index.js
@@ -3,17 +3,27 @@ const path = require("path");
 
 const app = express();
 
+/* 
 const pathDir = path.join(__dirname, "public");
 app.use(express.static(pathDir));   // always find the index file and show the result as default page
+ */
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.sendFile(`${pathDir}/index.html`);
+  // res.sendFile(`${pathDir}/index.html`);
+  res.render("index", {
+    title: "Index Page",
+  });
 });
-app.get("/about", (req, res) => {
-  res.sendFile(`${pathDir}/home.html`);
+app.get("/home", (req, res) => {
+  // res.sendFile(`${pathDir}/home.html`);
+  res.render("home", {
+    title: "Home Page",
+  });
 });
 app.get("/download", (req, res) => {
-  res.download(`${pathDir}/index.html`);
+  // res.download(`${pathDir}/index.html`);
 });
 
 app.listen(3000, () => {
